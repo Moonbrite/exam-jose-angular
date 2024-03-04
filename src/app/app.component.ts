1df64bca1cc582a828308b30608dc86a1833f108
Moonbrite <josemiguelvalecosta@gmail.com>
2024-03-04 22:38:15 +0100

    ajout du loader et modif du readMe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from "./components/blocks/header/header.component";
+import {LoaderComponent} from "./components/blocks/loader/loader.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, LoaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
