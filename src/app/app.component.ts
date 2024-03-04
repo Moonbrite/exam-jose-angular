40120351270f66a65f881f9ed36ae0246e7a36f2
Moonbrite <josemiguelvalecosta@gmail.com>
2024-03-04 23:12:17 +0100

    ajout Footer, loader edit header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from "./components/blocks/header/header.component";
 import {LoaderComponent} from "./components/blocks/loader/loader.component";
+import {FooterComponent} from "./components/blocks/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, LoaderComponent],
+  imports: [RouterOutlet, HeaderComponent, LoaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/components/blocks/header/header.component.ts
+++ src/app/components/blocks/header/header.component.ts
@@ -1,10 +1,13 @@
 import {Component, OnInit} from '@angular/core';
 import {NgbCollapse} from "@ng-bootstrap/ng-bootstrap";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
